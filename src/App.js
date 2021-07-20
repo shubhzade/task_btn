96f2c91f0b2d77f2a02d8e9b9c2ed118a35d5d47
shubhzade <https://github.com/shubham-4bits/vuxy-form.git>
2021-07-21 01:03:01 +0530

    taskb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+// import "./styles.css";
+import "./App.css";
 
-function App() {
+//☀︎ ☽
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [darkModa, setDarkModa] = useState(false);
+  const [darkModb, setDarkModb] = useState(false);
+  const [darkModee, setDarkModee] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={darkMode ? "dark-mode" : "light-mode"}>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+          <div>
+            <h1> Its {darkMode ? "Dark" : "Black"} Mode </h1>
+          </div>
+        </div>
+
+        <div className={darkModee ? "dark-modee" : "light-modee"}>
+          <div className="switch-checkbox">
+            <label className="switch">
+              <input
+                type="checkbox"
+                onChange={() => setDarkModee(!darkModee)}
+              />
+              <span className="slider round"> </span>
+            </label>
+          </div>
+          <h1> Its {darkModee ? "Light" : "Dark"} Mode </h1>
+          <div className={darkModa ? "dark-moda" : "light-moda"}>
+            {/* ------------------------------- */}
+
+            <div className="switch-checkbox">
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  onChange={() => setDarkModa(!darkModa)}
+                />
+                <span className="slider round"> </span>
+              </label>
+              <div>
+                <h1>Its {darkModa ? "Dark" : "Light"} Mode </h1>
+              </div>
+              <div className={darkModb ? "dark-modb" : "light-modb"}>
+                <div className="switch-checkbox">
+                  <label className="switch">
+                    <input
+                      type="checkbox"
+                      onChange={() => setDarkModb(!darkModb)}
+                    />
+                    <span className="slider round"> </span>
+                  </label>
+                </div>
+                <h1>Its {darkModb ? "Dark" : "Light"} Mode </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
